a5234cd8c3325be127f8689c310c75b01a9670e9
Egor <Egor@DESKTOP-CMV1REH>
2023-11-24 23:23:59 +0300

    Добавили навигацию между окнами и страницами

---
+++ ViplataProject/Pages/ClientsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ViplataProject.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientsPage.xaml
+    /// </summary>
+    public partial class ClientsPage : Page
+    {
+        public ClientsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+           // NavigationService.Navigate(new MenuPage());
+            ClientsPage clientsPage = new ClientsPage();
+            MainWindow mainWindow = (MainWindow)Window.GetWindow(this);
+            mainWindow.Title = "Главное меню";
+            NavigationService.Navigate(new MenuPage());
+        }
+    }
+}
