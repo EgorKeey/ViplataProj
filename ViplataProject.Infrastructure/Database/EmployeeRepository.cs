8b9737d5eddcffe7f50267d07eb1b863adb6ad8c
keecth <gusarov.du@gmail.ocm>
2023-11-26 11:54:37 +0300

    Выполнил подготовку и начал выполнять по 2-ому подходу 1 задание

---
+++ ViplataProject.Infrastructure/Database/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViplataProject.Infrastructure.Database
+{
+    internal class EmployeeRepository
+    {
+    }
+}
